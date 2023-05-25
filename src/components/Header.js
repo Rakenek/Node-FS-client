33b8bca06687db6af07e8dfe9a49a9d9bce41867
Rakenek <kamilotawski@gmail.com>
2023-05-25 23:24:04 +0200

    123

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,6 +22,9 @@ const Header = () => {
           <li key={1}>
             <Payments />
           </li>,
+          <li key={3} style={{ margin: '0 10px' }}>
+            Credits:{auth.credits}
+          </li>,
           <li key={2}>
             <a href="/api/logout">Logout</a>
           </li>,
--- src/components/Payments.js
+++ src/components/Payments.js
@@ -1,12 +1,16 @@
+import { useDispatch } from 'react-redux';
 import StripeCheckout from 'react-stripe-checkout';
 
+import { handleToken } from '../actions';
+
 const Payments = () => {
+  const dispatch = useDispatch();
   return (
     <StripeCheckout
       name="Emaily"
       description="$5 for 5 email credits"
       amount={500}
-      token={(token) => console.log(token)}
+      token={(token) => dispatch(handleToken(token))}
       stripeKey={process.env.REACT_APP_STRIPE_KEY}
     >
       <button className="btn">Add Credits</button>
